157fa7e39ec4023b69e1283330bbde0b7ad7fe88
Mba Thomas <angelfab631@gmail.com>
2022-07-19 05:16:03 +0100

    Create Pages.php
    This is a class that handles the pages methods and extends the Controller class

---
+++ app/controllers/Pages.php
@@ -0,0 +1,25 @@
+<?php
+class Pages extends Controller
+{
+   public function __construct()
+   {
+      // echo 'Pages Loaded';
+   }
+
+   public function index()
+   {
+      $data = [
+         'title' => 'SharePosts',
+         'description' => 'Simple social network built on the SharePost-MVC PHP framework',
+      ];
+      $this->view('pages/index', $data);
+   }
+   public function about()
+   {
+      $data = [
+         'title' => 'About Us',
+         'description' => 'This App is built to share posts with other users',
+      ];
+      $this->view('pages/about', $data);
+   }
+}
