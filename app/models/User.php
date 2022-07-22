6acbf45c31d25f47c65cc5e45396298f5a7b1dc2
Mba Thomas <angelfab631@gmail.com>
2022-07-22 01:12:13 +0100

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -7,6 +7,23 @@ class User {
       $this->db = new Database;
    }
 
+   // Register user
+
+   public function register($data) {
+      $this->db->query('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+      // Bind values
+      $this->db->bind(':name', $data['name']);
+      $this->db->bind(':email', $data['email']);
+      $this->db->bind(':password', $data['password']);
+
+      // Execute query
+      if($this->db->execute()) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+
    // Validate User email
    public function findUserEmail($email) {
       $this->db->query('SELECT * FROM users WHERE email = :email');
