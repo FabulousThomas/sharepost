25d4e8b40bafa5265df604e1cc013792f7eaaa76
Mba Thomas <angelfab631@gmail.com>
2022-07-28 09:55:52 +0100

    Update Posts.php

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,21 +1,61 @@
-<?php 
-class Posts extends Controller {
+<?php
+class Posts extends Controller
+{
 
    public function __construct()
    {
-      if(!isLoggedIn()) {
+      if (!isLoggedIn()) {
          redirect('users/login');
       }
 
       $this->postModel = $this->model('Post');
    }
 
-   public function index() {
+   public function index()
+   {
       $posts = $this->postModel->getPosts();
-      $data = [
-         'posts' => $posts,
-      ];
-      $this->view('posts/index', $data);
+      // Check for Post Request
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+         // die('Post is added');
+         // Sanitize post input
+         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+         $data = [
+            'posts' => $posts,
+            'title' => trim($_POST['title']),
+            'body' => trim($_POST['body']),
+            'user_id' => $_SESSION['user_id'],
+            'title_err' => '',
+            'body_err' => '',
+         ];
+         // Validate title
+         if(empty($data['title'])) {
+            $data['title_err'] = 'Please, enter title';
+         }
+         // Validate body
+         if(empty($data['body'])) {
+            $data['body_err'] = 'Please, enter body';
+         }
+         if(empty($data['title_err']) && empty($data['body_err'])) {
+            // SUCCESS
+            die('FORM SUBMITED');
+         } else {
+            $this->view('posts/index', $data);
+         }
+      } else {
+         
+         $data = [
+            'posts' => $posts,
+            'title' => '',
+            'body' => '',
+            'title_err' => '',
+            'body_err' => '',
+         ];
+         $this->view('posts/index', $data);
+      }
+
+         // $data = [
+         //    'posts' => $posts,
+         // ];
+         // $this->view('posts/index', $data);
    }
-   
 }
