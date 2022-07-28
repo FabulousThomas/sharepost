9f4bdc1a65565796db944863559c491cad9bbbea
Mba Thomas <angelfab631@gmail.com>
2022-07-28 09:56:08 +0100

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -10,7 +10,16 @@ class Post extends Controller
 
    public function getPosts()
    {
-      $this->db->query('SELECT * posts.id as postId, users.id as userId, FROM posts INNER JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC');
+      $this->db->query('SELECT *,
+                        posts.id as postId,
+                        users.id as userId,
+                        posts.created_at as postCreated,
+                        users.created_at as userCreated
+                        FROM posts
+                        INNER JOIN users
+                        ON posts.user_id = users.id
+                        ORDER BY posts.created_at DESC
+                        ');
       $result = $this->db->resultSet();
       return $result;
    }
