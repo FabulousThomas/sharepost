b44df93b37c7eb74011c496186fd5d28c7f8af6f
Mba Thomas <angelfab631@gmail.com>
2022-07-30 02:09:14 +0100

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -38,13 +38,42 @@ class Post extends Controller
       }
    }
 
+   // Get Post by ID
    public function getPostId($id)
    {
       $this->db->query('SELECT * FROM posts WHERE id = :id');
       $this->db->bind(':id', $id);
 
       $row = $this->db->singleSet();
-      
+
       return $row;
    }
+
+     // Update posts
+     public function updatePosts($data)
+   {
+      $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+      $this->db->bind(':id', $data['id']);
+      $this->db->bind(':title', $data['title']);
+      $this->db->bind(':body', $data['body']);
+
+      if ($this->db->execute()) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+      // Get Post by ID
+      public function deletePosts($id)
+      {
+         $this->db->query('DELETE FROM posts WHERE id = :id');
+         $this->db->bind(':id', $id);
+   
+         if ($this->db->execute()) {
+            return true;
+         } else {
+            return false;
+         }
+      }
 }
