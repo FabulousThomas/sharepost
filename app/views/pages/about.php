0631e7e39e8650ba7e8eea4b665e4ad2bdde1c30
Mba Thomas <angelfab631@gmail.com>
2022-08-20 04:21:37 +0100

    Update about.php

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-   <h1 class="display-4"><?php echo $data['title']; ?></h1>
-   <p class="lead"><?php echo $data['description']; ?></p>
-   <small>Version: <strong><?php echo APPVERSION; ?></strong></small>
+<h1 class="display-4"><?php echo $data['title']; ?></h1>
+<p class="lead"><?php echo $data['description']; ?></p>
+<small>Version: <strong><?php echo APPVERSION; ?></strong></small>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
